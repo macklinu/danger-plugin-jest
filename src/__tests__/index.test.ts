4e75936cf978fdf609557dce50fc7717653179c4
stereobooster <stereobooster@gmail.com>
2019-03-14 11:50:34 +0100

    chore(package): update jest and typescript

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -4,58 +4,60 @@ import jestResults from "..";
 
 // tslint:disable:no-string-literal
 
-beforeEach(() => {
-  global["message"] = jest.fn();
-  global["fail"] = jest.fn();
-  global["warn"] = jest.fn();
-  global["danger"] = {
-    github: {
-      pr: {
-        head: {
-          ref: "branch",
-          repo: {
-            full_name: "repo/slug"
+describe("jestResults", () => {
+  beforeEach(() => {
+    global["message"] = jest.fn();
+    global["fail"] = jest.fn();
+    global["warn"] = jest.fn();
+    global["danger"] = {
+      github: {
+        pr: {
+          head: {
+            ref: "branch",
+            repo: {
+              full_name: "repo/slug"
+            }
           }
+        },
+        utils: {
+          fileLinks: <T>(a: T) => a
         }
-      },
-      utils: {
-        fileLinks: a => a
       }
-    }
-  };
-});
-
-test("messages with passing test results", () => {
-  jestResults({
-    testResultsJsonPath: fixture("passing-tests.json")
+    };
   });
-  expect(global["fail"]).not.toHaveBeenCalled();
-});
 
-test("fails with failing test results", () => {
-  jestResults({
-    testResultsJsonPath: fixture("failing-tests.json")
+  it("messages with passing test results", () => {
+    jestResults({
+      testResultsJsonPath: fixture("passing-tests.json")
+    });
+    expect(global["fail"]).not.toHaveBeenCalled();
   });
-  expect(global["fail"]).toHaveBeenCalledWith(expect.stringMatching(/FAIL/));
-});
 
-test("fails with failing test results from jest 20", () => {
-  jestResults({
-    testResultsJsonPath: fixture("failed-tests-j20.json")
+  it("fails with failing test results", () => {
+    jestResults({
+      testResultsJsonPath: fixture("failing-tests.json")
+    });
+    expect(global["fail"]).toHaveBeenCalledWith(expect.stringMatching(/FAIL/));
   });
-  expect(global["fail"]).toHaveBeenCalledWith(expect.stringMatching(/FAIL/));
-});
 
-test("warns when test results JSON file cannot be read", () => {
-  jestResults({
-    testResultsJsonPath: fixture("nonexistent.json")
+  it("fails with failing test results from jest 20", () => {
+    jestResults({
+      testResultsJsonPath: fixture("failed-tests-j20.json")
+    });
+    expect(global["fail"]).toHaveBeenCalledWith(expect.stringMatching(/FAIL/));
   });
-  expect(global["fail"]).toHaveBeenCalled();
-});
 
-test.skip("Fails 6", () => {
-  expect({ v: "asda" }).toContain("s");
-});
+  it("warns when test results JSON file cannot be read", () => {
+    jestResults({
+      testResultsJsonPath: fixture("nonexistent.json")
+    });
+    expect(global["fail"]).toHaveBeenCalled();
+  });
 
-const fixture = (filename: string) =>
-  path.resolve(__dirname, "__fixtures__", filename);
+  it.skip("Fails 6", () => {
+    expect({ v: "asda" }).toContain("s");
+  });
+
+  const fixture = (filename: string) =>
+    path.resolve(__dirname, "__fixtures__", filename);
+});
